6d90c25508a73722a30c5f38e165c58f0a89bd04
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-11-29 16:06:45 +0700

    cls

--- src/assets/seasonal_assets.ts
+++ src/assets/seasonal_assets.ts
@@ -4,30 +4,30 @@ export const seasonAssets: SeasonalAssets = {
   spring: {
     images: [],
     videos: [
-      ...Array.from({ length: 5 }, (_, i) => {
+      ...Array.from({ length: 7 }, (_, i) => {
+        if (i === 5) return "/videos/season_bg/spring/spring_6.mov";
         return `/videos/season_bg/spring/spring_${i + 1}.mp4`;
       }),
-      "/videos/season_bg/spring/spring_6.mov",
     ],
     title: ["Spring Story", "Spring Memories"],
   },
   summer: {
     images: [],
-    videos: Array.from({ length: 8 }, (_, i) => {
+    videos: Array.from({ length: 10 }, (_, i) => {
       return `/videos/season_bg/summer/summer_${i + 1}.mp4`;
     }),
     title: ["Summer Story", "Summer Memories"],
   },
   fall: {
     images: [],
-    videos: Array.from({ length: 6 }, (_, i) => {
+    videos: Array.from({ length: 8 }, (_, i) => {
       return `/videos/season_bg/autumn/autumn_${i + 1}.mp4`;
     }),
     title: ["Fall Story", "Fall Memories"],
   },
   winter: {
     images: [],
-    videos: Array.from({ length: 8 }, (_, i) => {
+    videos: Array.from({ length: 9 }, (_, i) => {
       return `/videos/season_bg/winter/winter_${i + 1}.mp4`;
     }),
     title: ["Winter Story", "Winter Memories"],
