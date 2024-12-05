98da08f53162e4ecebbb65376bea1d60942676c5
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-12-05 16:38:37 +0700

    deps: install @remotion/transitions for chapter + frame animation

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,11 +1,37 @@
 import { AbsoluteFill } from "remotion";
+import { linearTiming, TransitionSeries } from "@remotion/transitions";
+import { none } from "@remotion/transitions/none";
 
 const MainScene = () => {
   return (
-    <AbsoluteFill>
-      <div className="flex items-center justify-center w-full h-full">
-        <h1 className="text-4xl font-bold text-white">Main Content here 😃</h1>
-      </div>
+    <TransitionSeries>
+      <TransitionSeries.Sequence durationInFrames={40}>
+        <Letter color="#0b84f3">A</Letter>
+      </TransitionSeries.Sequence>
+      <TransitionSeries.Transition
+        presentation={none()}
+        timing={linearTiming({ durationInFrames: 30 })}
+      />
+      <TransitionSeries.Sequence durationInFrames={60}>
+        <Letter color="pink">B</Letter>
+      </TransitionSeries.Sequence>
+    </TransitionSeries>
+  );
+};
+
+const Letter: React.FC<{ color: string; children: React.ReactNode }> = ({
+  color,
+  children,
+}) => {
+  return (
+    <AbsoluteFill
+      style={{
+        backgroundColor: "transparent ",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ color, fontSize: 200 }}>{children}</h1>
     </AbsoluteFill>
   );
 };
